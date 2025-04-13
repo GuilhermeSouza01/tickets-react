6191e7a64508fa547328546ddfda7b64d308c76f
Guilherme Souza <ghostgui60@gmail.com>
2025-04-13 13:53:08 -0300

    feat: add eslint to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/themes/global";
