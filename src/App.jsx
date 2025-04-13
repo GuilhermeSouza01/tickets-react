ae7408db9f6a8ed66ef6aa7246bc56ce08141ad6
Guilherme Souza <ghostgui60@gmail.com>
2025-04-13 12:48:56 -0300

    feat: cleaning projects and setup styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={defaultTheme}>
+      <div style={{ backgroundColor: defaultTheme.backgroundLight, color: defaultTheme.textPrimary, padding: '20px' }}>
+        <h1 style={{ color: defaultTheme.primary }}>My React App</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
+
   )
 }
-
-export default App
---
+++ src/styles/themes/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['primary-700']};
+  }
+
+  body {
+    background-color: ${props => props.theme['background-100']};
+    color: ${props => props.theme['primary-100']};
+  }
+
+  body, input, text-area, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
