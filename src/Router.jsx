ce9ddc6b60a981e93d29d80c240f7d32c8f580f4
Guilherme Souza <ghostgui60@gmail.com>
2025-04-18 00:16:54 -0300

    feat: add tickets route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home/Home"
+import { Home } from "./pages/Home"
 import { DefaultLayout } from "./layouts/DefaultLayout"
+import { Tickets } from "./pages/Tickets"
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
+      <Route path="/tickets" element={<Tickets />} />
       </Route>
     </Routes>
   )
