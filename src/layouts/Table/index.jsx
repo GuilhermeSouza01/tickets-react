4c2bc4e21548f792ab2cfd7f03bf7485ae2c2dab
Guilherme Souza <ghostgui60@gmail.com>
2025-04-21 16:38:31 -0300

    feat: create table component and styles

--- src/layouts/Table/index.jsx
+++ src/layouts/Table/index.jsx
@@ -1,38 +1,15 @@
+/* eslint-disable react/prop-types */
 
 import React from 'react';
 
-import { StyledTable, StyledTableBody, StyledTableHeader, StyledTableRow } from './styles';
+import { Body, DataRow, Header, StyledTable } from './styles';
 
-export function Table() {
+export function Table({children}) {
 
   return (
     <StyledTable>
-      <StyledTableHeader>
-        <h2>Table Header</h2>
-        <button>Add</button>
-      </StyledTableHeader>
-      <StyledTableBody>
-        <StyledTableRow>
-          <p>Row 1</p>
-          <p>Row 2</p>
-          <p>Row 3</p>
-        </StyledTableRow>
-        <StyledTableRow>
-          <p>Row 4</p>
-          <p>Row 5</p>
-          <p>Row 6</p>
-        </StyledTableRow>
-        <StyledTableRow>
-          <p>Row 7</p>
-          <p>Row 8</p>
-          <p>Row 9</p>
-        </StyledTableRow>
-        <StyledTableRow>
-          <p>Row 10</p>
-          <p>Row 11</p>
-          <p>Row 12</p>
-        </StyledTableRow>
-        </StyledTableBody>
+
+       {children}
     </StyledTable>
   );
 }
--- src/layouts/Table/styles.js
+++ src/layouts/Table/styles.js
@@ -1,32 +1,40 @@
 import styled from "styled-components";
 
 export const StyledTable = styled.div`
-  border: 1px solid ${(props) => props.theme["primary-500"]};
+  border: 1px solid ${(props) => props.theme["color-grey-300"]};
 
-  font-size: 1.5rem;
-  background-color: ${(props) => props.theme["background-100"]};
+  font-size: 0.9rem ;
+  background-color: ${(props) => props.theme["background-50"]};
   border-radius: 7px;
   overflow: hidden;
 `;
 
-export const StyledTableHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr 1fr;
+  column-gap: 2rem;
   align-items: center;
-  padding: 1rem;
-  background-color: ${(props) => props.theme["primary-500"]};
-  color: ${(props) => props.theme["background-100"]};
 `;
-export const StyledTableBody = styled.div`
-  padding: 1rem;
-  background-color: ${(props) => props.theme["background-100"]};
+
+export const Header = styled(Row)`
+  padding: 1.6rem 2.4rem;
+  background-color: ${(props) => props.theme["color-tone-blue-100"]};
+  color: ${(props) => props.theme["color-grey-500"]};
+  border-bottom: 1px solid ${(props) => props.theme["color-grey-300"]};
+  text-transform: uppercase;
+  letter-spacing: 0.4px;
+  font-weight: 600;
 `;
-export const StyledTableRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+export const Body = styled.div`
   padding: 1rem;
-  border-bottom: 1px solid ${(props) => props.theme["primary-500"]};
+  background-color: ${(props) => props.theme["background-50"]};
+`;
+export const DataRow = styled(Row)`
+ padding: 1.2rem 2.4rem;
+
+&:not(:last-child) {
+  border-bottom: 1px solid ${(props) => props.theme["color-grey-300"]};
+}
 `;
 
 export const EmptyTable = styled.p`
@@ -34,4 +42,5 @@ export const EmptyTable = styled.p`
   text-align: center;
   font-weight: 500;
   margin: 2rem 0;
+  color: ${(props) => props.theme["color-grey-500"]};
 `;
