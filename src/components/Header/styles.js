b98490d7d8ab91b23971a85480f9d6fd16fa39b2
Guilherme Souza <ghostgui60@gmail.com>
2025-04-21 16:36:15 -0300

    fix: change the header color

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,6 +7,6 @@ export const StyledHeader = styled.header`
   justify-content: space-between;
   padding: 1.2rem 4.8rem;
   background-color: ${ props => props.theme['color-tone-blue-100']};
-  border-bottom: 1px solid ${ props => props.theme['color-grey-100']};
+  border-bottom: 1px solid ${ props => props.theme['color-grey-300']};
   gap: 2.4rem;
 `;
