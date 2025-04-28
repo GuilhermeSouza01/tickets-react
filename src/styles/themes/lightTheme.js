bd4be48a9dfd6af70da3ea3677d0569e9f5d0e6b
Guilherme Souza <ghostgui60@gmail.com>
2025-04-28 18:57:16 -0300

    feat: Change some theme default colors and add dark mode variables

---
+++ src/styles/themes/lightTheme.js
@@ -0,0 +1,59 @@
+export const lightTheme = {
+
+  "background-50": "#eaefff",
+
+  "color-grey-0": "#fff",
+  "color-grey-50": "#f9fafb",
+  "color-grey-100": "#f3f4f6",
+  "color-grey-200": "#e5e7eb",
+  "color-grey-300": "#d1d5db",
+  "color-grey-400": "#9ca3af",
+  "color-grey-500": "#6b7280",
+  "color-grey-600": "#4b5563",
+  "color-grey-700": "#374151",
+  "color-grey-800": "#1f2937",
+  "color-grey-900": "#111827",
+
+  "color-blue-100": "#e0f2fe",
+  "color-blue-700": "#0369a1",
+  "color-green-100": "#dcfce7",
+  "color-green-700": "#15803d",
+  "color-yellow-100": "#fef9c3",
+  "color-yellow-700": "#a16207",
+  "color-silver-100": "#e5e7eb",
+  "color-silver-700": "#374151",
+
+
+  //great-blue
+
+  "color-brand-50": "#E3E9F8",
+  "color-brand-100": "#C3D1EF",
+  "color-brand-200": "#86A2DF",
+  "color-brand-300": "#4A74CF",
+  "color-brand-400": "#2B50A1",
+  "color-brand-500": "#1B3366",
+  "color-brand-600": "#152851",
+  "color-brand-700": '#101E3C',
+  "color-brand-800": '#0B1428',
+  "color-brand-900": '#050A14',
+
+
+
+  "color-red-100": "#fee2e2",
+  "color-red-700": "#b91c1c",
+  "color-red-800": "#991b1b",
+
+  "backdrop-color": "rgba(255, 255, 255, 0.1)",
+
+  "shadow-sm": "0 1px 2px rgba(0, 0, 0, 0.04)",
+  "shadow-md": "0px 0.6rem 2.4rem rgba(0, 0, 0, 0.06)",
+  "shadow-lg": "0 2.4rem 3.2rem rgba(0, 0, 0, 0.12)",
+
+  "image-grayscale": "0",
+  "image-opacity": "100%",
+
+  "border-radius-tiny": "3px",
+  "border-radius-sm": "5px",
+  "border-radius-md": "7px",
+  "border-radius-lg": "9px",
+};
