bbbe6f827fbecd18af1b7271be5e8111a6e971fd
Guilherme Souza <ghostgui60@gmail.com>
2025-04-28 18:59:23 -0300

    fix: transition for dark mode is so fast

--- src/styles/themes/global.js
+++ src/styles/themes/global.js
@@ -2,16 +2,21 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
-  * {
+  *,
+  *::before,
+  *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    transition: background-color 0.3s, border 0.3s;
   }
 
   body {
     background-color: ${props => props.theme['background-50']};
     color: ${props => props.theme['color-grey-700']};
     min-height: 100vh;
+    transition: color 0.3s, background-color 0.3s;
   }
 
   body, input, text-area, button {
@@ -33,13 +38,23 @@ export const GlobalStyle = createGlobalStyle`
     cursor: not-allowed;
   }
 
+  select:disabled,
+  input:disabled {
+  background-color: ${props => props.theme['color-grey-200']};
+  color: ${props => props.theme['color-grey-500']};
+}
+
   input:focus,
   button:focus,
   textarea:focus,
   select:focus {
-  outline: 2px solid ${props => props.theme['color-brand-600']};
+  outline: 2px solid ${props => props.theme['color-brand-400']};
   outline-offset: -1px;
   }
+
+  button:has(svg) {
+  line-height: 0;
+}
 
   a {
     color: inherit;
