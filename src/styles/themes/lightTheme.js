221d309a42f8983ae7576a720f496d8e9b406abf
Guilherme Souza <ghostgui60@gmail.com>
2025-05-09 14:44:32 -0300

    fix: Change default blue color

--- src/styles/themes/lightTheme.js
+++ src/styles/themes/lightTheme.js
@@ -14,8 +14,8 @@ export const lightTheme = {
   "color-grey-800": "#1f2937",
   "color-grey-900": "#111827",
 
-  "color-blue-100": "#e0f2fe",
-  "color-blue-700": "#0369a1",
+  "color-blue-100": "#ebf2fe",
+  "color-blue-700": "#3B82F7",
   "color-green-100": "#dcfce7",
   "color-green-700": "#15803d",
   "color-yellow-100": "#fef9c3",
