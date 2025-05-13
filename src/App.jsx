abb313d58998b828bc691253e92e7f5c97e88337
Guilherme Souza <ghostgui60@gmail.com>
2025-05-12 23:39:27 -0300

    feat: add the toast to the app

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Router } from "./Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +27,26 @@ export function App() {
           <BrowserRouter>
             <Router />
           </BrowserRouter>
+          <Toaster
+            position="top-right"
+            gutter={12}
+            containerStyle={{ margin: "8px"}}
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 3000,
+              },
+              style: {
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                backgroundColor: `${lightTheme["color-grey-0"]}`,
+                color: `${lightTheme["color-grey-900"]}`,
+              }
+             }}
+          />
           <GlobalStyle />
     </QueryClientProvider>
     </DarkModeProvider>
