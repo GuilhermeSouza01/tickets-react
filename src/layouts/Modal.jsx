4b057d58c87da4137698618ece4e1ac99c104fc9
Guilherme Souza <ghostgui60@gmail.com>
2025-05-15 10:48:54 -0300

    feat: create modal component

---
+++ src/layouts/Modal.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+
+
+import { cloneElement, createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
+import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+
+const StyledModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${(props) => props.theme["color-grey-0"]};
+  border-radius: ${(props) => props.theme["border-radius-lg"]};
+  box-shadow: ${(props) => props.theme["shadow-lg"]};
+  padding: 3.2rem 4rem;
+  transition: all 0.5s;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.theme["backdrop-color"]};
+  backdrop-filter: blur(4px);
+  z-index: 1000;
+  transition: all 0.5s;
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  padding: 0.4rem;
+  border-radius: ${(props) => props.theme["border-radius-sm"]};
+  transform: translateX(0.8rem);
+  transition: all 0.2s;
+  position: absolute;
+  top: 1.2rem;
+  right: 1.9rem;
+
+  &:hover {
+    background-color: ${(props) => props.theme["color-grey-100"]};
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: ${(props) => props.theme["color-grey-500"]};
+  }
+`;
+
+
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
+  return createPortal(
+    <Overlay>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
+          <HiXMark />
+        </Button>
+
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
